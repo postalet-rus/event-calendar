cd10df9bae8b94af563021239d188ef4b5f5d479
postalet-rus <musieryot@gmail.com>
2020-09-06 22:47:46 +0500

    added features: events menu, table cells selection, settings menu

--- js/main.js
+++ js/main.js
@@ -1,28 +1,44 @@
 "use strict"
 
 // utility functions and shortcuts
-const $ = (x) => { return document.querySelector(x) } //get element by X selector
-const $all = (x) => { return document.querySelectorAll(x) } //get all elements by X selector
-const $new = (elementName, elementClass) => {
+const $ = (selector) => { return document.querySelector(selector) } // get element by X selector
+const $all = (selector) => { return document.querySelectorAll(selector) } // get all elements by X selector
+const $arr = (elementName) => {
+  if($(elementName) !== undefined) {
+    return Array.from($all(elementName))
+  }
+}
+const $new = (elementName, elementClass) => { // create X element
   let newElement = document.createElement(elementName);
   if (elementClass) {
     newElement.className = `${elementClass}`
   }
   return newElement
-} //create X element
-const DAYENTRY = (date, element, weekday) => {return {"date": date, "element": element, "weekday": weekday}}; //create day object
-const WEEKDAY = (day) => { //get weekday by day shortcut
+}
+const $posCenterX = (element) => { // returns x coordinates of elements center
+  let posX = element.getBoundingClientRect().x;
+  let width = element.getBoundingClientRect().width;
+  return posX + (width / 2)
+}
+const $posCenterY = (element) => { // returns y coordinates of elements center
+  let posY = element.getBoundingClientRect().y;
+  let height = element.getBoundingClientRect().height;
+  return posY + (height / 2)
+}
+const DAYENTRY = (date, element, weekday) => {return {"date": date, "element": element, "weekday": weekday}}; // create day object
+const WEEKDAY = (day) => { // get weekday by day shortcut
   let weekday = new Date(YEAR, month, day).getDay();
   weekday === 0 ? weekday = 7 : weekday;
   return weekday
 }
-const UTC = (yyyy, mm, dd) => { //parse date to UTC format YYYY-MM-DD
+const UTC = (yyyy, mm, dd) => { // parse date to UTC format YYYY-MM-DD
   mm < 10 ? mm = `0${mm+1}` : mm+1;
   dd < 10 ? dd = `0${dd}` : dd;
   return `${yyyy}-${mm}-${dd}`
 }
-const TODAY = (mm) => { //get today in UTC
+const TODAY = () => { // get today in UTC
   let day = new Date().getDate();
+  let mm = new Date().getMonth();
   return UTC(YEAR, mm, day);
 }
 
@@ -34,6 +50,7 @@ const YEAR = new Date().getFullYear();
 let calendarPlace = $("#cal_main");
 let days = new Array;
 let currentTD;
+
 
 //functions
 function createCalendarHeader() {
@@ -61,11 +78,13 @@ function fillCalendar(month) { //creates calendar body and inserts in table
     row.insertAdjacentElement("beforeEnd", cell);
     days.push(DAYENTRY(UTC(YEAR, month, day), cell, WEEKDAYS_SHORTCUTS[WEEKDAY(day)-1]))
   }
+  calendarPlace.insertAdjacentElement("beforeEnd", constructAndGetMenuBlock())
   return table
 }
 
+
 function highlightToday(month) {
-  let today = days.filter(item => item.date == TODAY(month))[0];
+  let today = days.filter(item => item.date == TODAY())[0];
   today.element.classList.add("today");
 }
 
@@ -83,25 +102,20 @@ function clearHighlightTH() {
   highlightedTH.classList.remove("active")
 }
  
-function showDayEventsMenu() { //shows menu
+function toggleDayEventsMenu() { //shows menu
   let menu = $(".event-menu");
-  if(!menu) {
-    menu = $("#cal_main").insertAdjacentElement("afterBegin", constructMenuBlock());
+  if(menu.classList.contains("hidden")) {
+    showMenu()
   } else {
-    clear();
-    menu = $("#cal_main").insertAdjacentElement("afterBegin", constructMenuBlock());
-  } return menu
+    $(".toggle-container").classList.add("toggle-container-minimized");
+    $(".event-menu").classList.add("minimized")
+  }
+  return menu
 }
 
 function bindEventListenerToElements(table) { // adding events
-
-  // clearAll
-  document.addEventListener("click", (event) => {
-    if(event.target.tagName != "TD") {
-      currentTD = null;
-      clear()
-    } 
-  });
+  let body = $("body");
+  let exceptionBlocks = [$("#s_expand_m"), $arr(".expanded-block"), $(".event-menu")]
 
   // mouseover
   table.addEventListener("mouseover", (event) => {
@@ -120,8 +134,7 @@ function bindEventListenerToElements(table) { // adding events
       return
     }
     currentTD = event.target;
-    let eventMenu = showDayEventsMenu();
-    setTimeout(() => eventMenu.classList.add("menu-expanded"), 1);
+    toggleDayEventsMenu();
   });
 }
 
@@ -149,41 +162,157 @@ window.addEventListener("keydown", (event) => { // shortcut
   }
 })
 
-function constructMenuBlock() { // create menu div block
-  let menu = $new("div", "event-menu");
+function constructAndGetMenuBlock() { // create menu div block
+  // creating elements
+  let menu = $new("div", "event-menu hidden");
   let menuWrapper = $new("div", "event-menu-wrapper");
+  let toggleContainer = $new("div", "toggle-container");
   let hr = $new("hr");
   let menuTitle = $new("h3", "menu-title");
   menuTitle.innerHTML = "События";
   let eventsContainer = $new("div", "events-container");
   eventsContainer.id = "events_container";
 
+  // build menu
   menu.insertAdjacentElement("beforeEnd", menuWrapper);
+  menuWrapper.insertAdjacentElement("afterBegin", toggleContainer);
   menuWrapper.insertAdjacentElement("beforeEnd", menuTitle);
   menuWrapper.insertAdjacentElement("beforeEnd", hr);
   menuWrapper.insertAdjacentElement("beforeEnd", eventsContainer);
 
-  menu.addEventListener("click", () => {
-    clear();
-    currentTD = null;
+  menu.addEventListener("click", (event) => {
+    if (toggleContainer.contains(event.target)) {
+      menu.classList.toggle("minimized");
+      toggleContainer.classList.toggle("toggle-container-minimized");
+    }
   });
   return menu
 }
 
 function hardClean() { // cleans events menu without animation
   let menu = $(".event-menu");
-  if (menu) menu.remove()
+  if (menu) menu.classList.add("hidden");
+  currentTD = undefined;
 }
 
-function clear() { // cleans events menu
+function showMenu() {
   let menu = $(".event-menu");
-  if (menu) {
+  if(menu.classList.contains("hidden")) menu.classList.remove("hidden");
+  menu.classList.add("menu-expanded");
+}
+
+function hideMenu() { // hides events menu
+  let menu = $(".event-menu");
+  if (menu.classList.contains("menu-expanded")) {
     menu.classList.remove("menu-expanded");
-    menu.addEventListener("transitionend", () => menu.remove());
+    menu.classList.add("hidden");
   }
+}
+
+
+// selection
+let selected = [];
+function startSelection() {
+  let xcoord, ycoord, finalX, finalY;
+  let table = $(".calendar-body");
+  table.addEventListener("mousedown", (event) => {
+    if ($(".event-menu").contains(event.target)) return
+    if (selected.length > 0) {
+      selected.forEach(item => {
+        item.element.classList.remove("selected-cell");
+      });
+      selected = []
+    }
+    xcoord = event.clientX;
+    ycoord = event.clientY;
+  })
+  table.addEventListener("mousemove", (event) => {
+  })
+  table.addEventListener("mouseup", (event) => {
+    if ($(".event-menu").contains(event.target)) return
+    finalX = event.clientX;
+    finalY = event.clientY;
+    finalX < xcoord ? finalX = [xcoord, xcoord = finalX][0] : finalX;
+    finalY < ycoord ? finalY = [ycoord, ycoord = finalY][0] : finalY;
+    for(let item of days) {
+      if (finalX >= $posCenterX(item.element) &&
+          $posCenterX(item.element) >= xcoord && 
+          finalY >= $posCenterY(item.element) &&
+          $posCenterY(item.element) >= ycoord) {
+        item.element.classList.add("selected-cell");
+        selected.push(item)
+      }
+     }
+  })
+  table.addEventListener("dragstart", (event) => event.preventDefault())
+}
+
+function bindEventListenersToSettings() {
+  let settingsBox = $("#settings");
+  let settingsList = $all(".settings-block");
+  if (settingsList.length == 0) {console.log("Settings blocks don't exists"); return false};
+  let settingsArray = Array.from(settingsList);
+  settingsBox.addEventListener("mouseout", (event) => {
+    if(settingsBox.contains(event.relatedTarget)) return
+    $("#s_expand_m").classList.add("hidden");        
+  })
+  settingsList.forEach(item => {
+    item.addEventListener("click", () => expandMenu(item));
+  })
+}
+
+function expandMenu(item) {
+  let settingsMenu = $("#s_expand_m"); 
+  let expandedBlocks = $arr(".expanded-block");
+  if (settingsMenu.classList.contains("hidden")) settingsMenu.classList.remove("hidden");
+  switch(true) {
+    case item.id === "s_date":
+      expandedBlocks.forEach(arrItem => {
+        if (arrItem.id === "es_date") {
+          arrItem.classList.remove("hidden");
+        } else if (!arrItem.classList.contains("hidden")) {
+          arrItem.classList.add("hidden")
+        }
+      })
+      break
+    case item.id === "s_range":
+      expandedBlocks.forEach(arrItem => {
+        if (arrItem.id === "es_range") {
+          arrItem.classList.remove("hidden");
+        } else if (!arrItem.classList.contains("hidden")) {
+          arrItem.classList.add("hidden")
+        }
+      })
+      break
+    case item.id === "s_utility":
+      expandedBlocks.forEach(arrItem => {
+        if (arrItem.id === "es_utility") {
+          arrItem.classList.remove("hidden");
+        } else if (!arrItem.classList.contains("hidden")) {
+          arrItem.classList.add("hidden")
+        }
+      })
+      break
+    case item.id === "s_notes":
+      expandedBlocks.forEach(arrItem => {
+        if (arrItem.id === "es_notes") {
+          arrItem.classList.remove("hidden");
+        } else if (!arrItem.classList.contains("hidden")) {
+          arrItem.classList.add("hidden")
+        }
+      })
+      break
+  }
+}
+
+function changeTheme(element) {
+  let body = $('body');
+  body.classList.toggle("black-themed")
 }
 
 //initialization//
 let generatedCalendar = calendarPlace.insertAdjacentElement("afterbegin", fillCalendar(month));
 bindEventListenerToElements(generatedCalendar);
 highlightToday(month);
+bindEventListenersToSettings()
+startSelection()
